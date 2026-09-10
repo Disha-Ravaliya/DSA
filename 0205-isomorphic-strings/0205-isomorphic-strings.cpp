711ee6019e40b7d61669d976a9dc90cac5ed4f60
Disha-Ravaliya <disharavaliya109@gmail.com>
2026-09-10 21:55:50 +0530

    Time: 3 ms (39.78%), Space: 9.5 MB (29.68%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // s[i] -> t[i] mapping
+            if (mp1.find(s[i]) != mp1.end()) {
+                if (mp1[s[i]] != t[i])
+                    return false;
+            }
+            else {
+                mp1[s[i]] = t[i];
+            }
+
+            // t[i] -> s[i] mapping
+            if (mp2.find(t[i]) != mp2.end()) {
+                if (mp2[t[i]] != s[i])
+                    return false;
+            }
+            else {
+                mp2[t[i]] = s[i];
+            }
+        }
+
+        return true;
+    }
+};
